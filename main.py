880631b626fea178321baaa3bd7c27144c5844c0
Thanawat Petchuen <thanawat1petchuen@gmail.com>
2020-02-22 00:08:08 +0700

    [ADD] Twitter authentication

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tweepy.streaming import StreamListener
+from requests_oauthlib import OAuth1Session
 from tweepy import OAuthHandler
 from tweepy import Stream
 import tweepy
 import json
 import pickle
+import twitter
 import operation
 
 def print_json(json_object):
@@ -18,15 +20,18 @@ def pickle_dump(x, name):
     pickle.dump([status._json for status in x], f)
 
 class Tweeper:
-  def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+  def __init__(self):
     self.files = {
       'timeline': 'timeline.pkl'
     }
-    self.initialize_api(consumer_key, consumer_secret, access_token, access_secret)
 
-  def initialize_api(self, consumer_key, consumer_secret, access_token, access_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
+    self.initialize_api()
+
+  def initialize_api(self):
+    twitter_auth = twitter.TwitterAuth()
+    key = twitter_auth.auth()
+    auth = tweepy.OAuthHandler(key['consumer_key'], key['consumer_secret'])
+    auth.set_access_token(key['access_token'], key['access_secret'])
     self.api = tweepy.API(auth, wait_on_rate_limit=True)
   
   def set_file(self, name, file):
@@ -54,11 +59,7 @@ def main():
   page = args['p'] or 10
   output = args['o']
   
-  consumer_key = 'Uu3FZb0UV4f86NOrcRXKoDWd6'
-  consumer_secret = 'iD3J7RF5I6h4zjNy6sy610g4WwbxHW9iGfgyAQxETYSszuYoB5'
-  access_token = '1052536547784847360-ebYM0ISRr59gcdW2Su7pfG4kOQlAsX'
-  access_secret = 'xvlCEUUZok0jbwGyLJoeBk4mQBXb8cCE76TDkz4dRcjfE'
-  client = Tweeper(consumer_key, consumer_secret, access_token, access_secret)
+  client = Tweeper()
   client.set_file('timeline', 'timeline3.pkl')
   client.get_home_timeline(page)
   operation.read_and_download('timeline3.pkl', output)
---
+++ twitter.py
@@ -0,0 +1,50 @@
+from requests_oauthlib import OAuth1Session
+import webbrowser
+
+class TwitterAuth:
+  def __init__(self):
+    self.consumer_key = 'Uu3FZb0UV4f86NOrcRXKoDWd6'
+    self.consumer_secret = 'iD3J7RF5I6h4zjNy6sy610g4WwbxHW9iGfgyAQxETYSszuYoB5'
+    self.resource_owner_key = ''
+    self.resource_owner_secret = ''
+  
+  def get_oauth_request_token(self):
+    request_token = OAuth1Session(client_key=self.consumer_key, client_secret=self.consumer_secret)
+    url = 'https://api.twitter.com/oauth/request_token'
+    data = request_token.get(url)
+    data_token = str.split(data.text, '&')
+    ro_key = str.split(data_token[0], '=')
+    ro_secret = str.split(data_token[1], '=')
+    self.resource_owner_key = ro_key[1]
+    self.resource_owner_secret = ro_secret[1]
+    auth_url = "https://api.twitter.com/oauth/authenticate?oauth_token={}".format(self.resource_owner_key)
+    print("Please open this link on your browser: ", auth_url)
+    webbrowser.open(auth_url, new=0, autoraise=True)
+
+  def get_oauth_token(self, verifier):
+    oauth_token = OAuth1Session(client_key=self.consumer_key,
+                                client_secret=self.consumer_secret,
+                                resource_owner_key=self.resource_owner_key,
+                                resource_owner_secret=self.resource_owner_secret)
+    url = 'https://api.twitter.com/oauth/access_token'
+    data = {"oauth_verifier": verifier}
+    access_token_data = oauth_token.post(url, data=data)
+    access_token_list = str.split(access_token_data.text, '&')
+    access_token_key = str.split(access_token_list[0], '=')
+    access_token_secret = str.split(access_token_list[1], '=')
+    access_token_name = str.split(access_token_list[3], '=')
+    access_token_id = str.split(access_token_list[2], '=')
+    key = access_token_key[1]
+    secret = access_token_secret[1]
+    name = access_token_name[1]
+    id = access_token_id[1]
+    return {"consumer_key": self.consumer_key, "consumer_secret": self.consumer_secret, "access_token": key, "access_secret": secret, "name": name, "id": id}
+
+  def auth(self):
+    self.get_oauth_request_token()
+    verifier = input("Verify code: ")
+    result = self.get_oauth_token(verifier)
+    return result
+
+if __name__ == '__main__':
+  print("Twitter Authentication (OAuth) module")
