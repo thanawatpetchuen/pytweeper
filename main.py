1b6a36f327eaee8e6121563440f875d7ea829141
Thanawat Petchuen <thanawat1petchuen@gmail.com>
2020-02-21 20:44:44 +0700

    [ADD] gitignore

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import json
 def print_json(json_object):
   print(json.dumps(json_object, indent=1))
 
+def write_json(dict):
+  with open('data.json', 'w+', encoding="utf8") as f:
+    json.dump([status._json for status in dict], f, indent=2)
+
 class Tweeper:
   def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
     self.initialize_api(consumer_key, consumer_secret, access_token, access_secret)
@@ -18,6 +22,9 @@ class Tweeper:
   
   def get_me(self):
     return self.api.me()
+  
+  def get_home_timeline(self, page=1):
+    return self.api.home_timeline(page=page)
 
 if __name__ == "__main__":
   name = '@BTS_SkyTrain'
@@ -26,4 +33,4 @@ if __name__ == "__main__":
   access_token = '1052536547784847360-ebYM0ISRr59gcdW2Su7pfG4kOQlAsX'
   access_secret = 'xvlCEUUZok0jbwGyLJoeBk4mQBXb8cCE76TDkz4dRcjfE'
   client = Tweeper(consumer_key, consumer_secret, access_token, access_secret)
-  print_json(client.get_me()._json)
+  write_json(client.get_home_timeline())
