13b0fdac93af03ad5d6ae3b8ca24990a59cd3019
Thanawat Petchuen <thanawat1petchuen@gmail.com>
2020-02-21 20:05:02 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import tweepy
+import json
+
+def print_json(json_object):
+  print(json.dumps(json_object, indent=1))
+
+class Tweeper:
+  def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+    self.initialize_api(consumer_key, consumer_secret, access_token, access_secret)
+
+  def initialize_api(self, consumer_key, consumer_secret, access_token, access_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    self.api = tweepy.API(auth, wait_on_rate_limit=True)
+  
+  def get_me(self):
+    return self.api.me()
+
+if __name__ == "__main__":
+  name = '@BTS_SkyTrain'
+  consumer_key = 'Uu3FZb0UV4f86NOrcRXKoDWd6'
+  consumer_secret = 'iD3J7RF5I6h4zjNy6sy610g4WwbxHW9iGfgyAQxETYSszuYoB5'
+  access_token = '1052536547784847360-ebYM0ISRr59gcdW2Su7pfG4kOQlAsX'
+  access_secret = 'xvlCEUUZok0jbwGyLJoeBk4mQBXb8cCE76TDkz4dRcjfE'
+  client = Tweeper(consumer_key, consumer_secret, access_token, access_secret)
+  print_json(client.get_me()._json)
