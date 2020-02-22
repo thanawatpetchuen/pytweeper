cfc9c54d4ae20950ec7dcd0116fce1c8349ca585
Thanawat Petchuen <thanawat1petchuen@gmail.com>
2020-02-22 22:55:51 +0700

    [ADD] Error handler and init consumer keys

--- pytweeper/pytweeper.py
+++ pytweeper/pytweeper.py
@@ -21,13 +21,28 @@ def load_key():
     with open('key.pkl', 'rb') as f:
       return pickle.load(f)
   except FileNotFoundError:
-    return None
+    return {}
 
-def delete_key():
+def remove_access_token():
   try:
-    os.remove('key.pkl')
+    with open('key.pkl', 'rb') as f:
+      key = pickle.load(f)
+    del key['access_token']
+    del key['access_secret']
+    del key['name']
+    del key['id']
+    del key['error']
+    with open('key.pkl', 'wb') as f:
+      pickle.dump(key, f)
     return True
   except FileNotFoundError:
+    return False
+
+def check_key(obj, key):
+  try:
+    if obj[key]:
+      return True
+  except KeyError:
     return False
 
 class Tweeper:
@@ -36,22 +51,27 @@ class Tweeper:
       'timeline': 'timeline.pkl'
     }
     self.authorize = False
+    self.twitter_auth = TwitterAuth()
   
   def logout(self):
-    if delete_key():
+    if remove_access_token():
       self.authorize = False
 
   def initialize_api(self):
-    twitter_auth = TwitterAuth()
-    key = load_key() or twitter_auth.auth()
-    if (not key['error']):
-      self.authorize = True
-      save_key(key)
-      auth = tweepy.OAuthHandler(key['consumer_key'], key['consumer_secret'])
-      auth.set_access_token(key['access_token'], key['access_secret'])
-      self.api = tweepy.API(auth, wait_on_rate_limit=True)
+    # TODO Initialize by given consumer keys
+    key = load_key()
+    if check_key(key, 'consumer_key') and check_key(key, 'consumer_secret'):
+      self.twitter_auth.set_comsumer_keys(key['consumer_key'], key['consumer_secret'])
+      if not(check_key(key, 'access_token') or check_key(key, 'access_secret')): # Already has keys
+        key = self.twitter_auth.auth()
+      if not key['error']:
+        save_key(key)
+        auth = tweepy.OAuthHandler(key['consumer_key'], key['consumer_secret'])
+        auth.set_access_token(key['access_token'], key['access_secret'])
+        self.api = tweepy.API(auth, wait_on_rate_limit=True)
+        self.authorize = True
     else:
-      print("Unauthorize")
+      print("Unauthorize please set consumer keys by run 'pytweeper init'")
   
   def set_file(self, name, file):
     self.files[name] = file
@@ -72,23 +92,34 @@ class Tweeper:
 def main():
   import argparse
   parser = argparse.ArgumentParser(description='PyTweeper (Twitter crawler)')
+  parser.add_argument('command', nargs='?', help='command or mode')
   parser.add_argument('-p', type=int, help='page count for crawler')
   parser.add_argument('-o', type=str, help='output destination')
   parser.add_argument('--logout', action='store_true', help='output destination')
 
   args = vars(parser.parse_args())
+  command = args['command']
   page = args['p'] or 10
   output = args['o'] or os.path.join(os.getcwd(), 'pytweeper','images')
   logout = args['logout']
-  
+
+  consumer_key = None
+  consumer_secret = None
+
   client = Tweeper()
   if logout:
     client.logout()
-  client.initialize_api()
-  if client.authorize:
-    client.set_file('timeline', 'timeline3.pkl')
-    client.get_home_timeline(page)
-    read_and_download('timeline3.pkl', output)
+  elif command == 'init':
+    consumer_key = input("Consumer key: ")
+    consumer_secret = input("Consumer secret: ")
+    with open('key.pkl', 'wb') as f:
+      pickle.dump({ "consumer_key": consumer_key, "consumer_secret": consumer_secret }, f)
+  else:
+    client.initialize_api()
+    if client.authorize:
+      client.set_file('timeline', 'timeline.pkl')
+      client.get_home_timeline(page)
+      read_and_download('timeline.pkl', output)
 
 
 if __name__ == "__main__":
--- pytweeper/twitter.py
+++ pytweeper/twitter.py
@@ -1,28 +1,34 @@
 from requests_oauthlib import OAuth1Session
 import webbrowser
 
-consumer_key_default ='Uu3FZb0UV4f86NOrcRXKoDWd6'
-consumer_secret_default = 'iD3J7RF5I6h4zjNy6sy610g4WwbxHW9iGfgyAQxETYSszuYoB5'
-
 class TwitterAuth:
-  def __init__(self, consumer_key=consumer_key_default, consumer_secret=consumer_secret_default):
-    self.consumer_key = consumer_key
-    self.consumer_secret = consumer_secret
+  def __init__(self):
+    self.consumer_key = ''
+    self.consumer_secret = ''
     self.resource_owner_key = ''
     self.resource_owner_secret = ''
+
+  def set_comsumer_keys(self, consumer_key, consumer_secret):
+    self.consumer_key = consumer_key
+    self.consumer_secret = consumer_secret
   
   def get_oauth_request_token(self):
     request_token = OAuth1Session(client_key=self.consumer_key, client_secret=self.consumer_secret)
     url = 'https://api.twitter.com/oauth/request_token'
     data = request_token.get(url)
-    data_token = str.split(data.text, '&')
-    ro_key = str.split(data_token[0], '=')
-    ro_secret = str.split(data_token[1], '=')
-    self.resource_owner_key = ro_key[1]
-    self.resource_owner_secret = ro_secret[1]
-    auth_url = "https://api.twitter.com/oauth/authenticate?oauth_token={}".format(self.resource_owner_key)
-    print("Please open this link on your browser: ", auth_url)
-    webbrowser.open(auth_url, new=0, autoraise=True)
+    if data.status_code == 200:
+      data_token = str.split(data.text, '&')
+      ro_key = str.split(data_token[0], '=')
+      ro_secret = str.split(data_token[1], '=')
+      self.resource_owner_key = ro_key[1]
+      self.resource_owner_secret = ro_secret[1]
+      auth_url = "https://api.twitter.com/oauth/authenticate?oauth_token={}".format(self.resource_owner_key)
+      print("Please open this link on your browser: ", auth_url)
+      webbrowser.open(auth_url, new=0, autoraise=True)
+      return True
+    else:
+      print("Consumer tokens are invalid please set consumer keys again.")
+      return False
 
   def get_oauth_token(self, verifier):
     oauth_token = OAuth1Session(client_key=self.consumer_key,
@@ -44,13 +50,19 @@ class TwitterAuth:
       id = access_token_id[1]
       return {"consumer_key": self.consumer_key, "consumer_secret": self.consumer_secret, "access_token": key, "access_secret": secret, "name": name, "id": id, "error": False}
     else:
-      return {"error": "Unauthorize"}
+      error = "Verfy code is invalid please try again."
+      print(error)
+      return {"error": error}
 
   def auth(self):
-    self.get_oauth_request_token()
-    verifier = input("Verify code: ")
-    result = self.get_oauth_token(verifier)
-    return result
+    if self.get_oauth_request_token():
+      try:
+        verifier = input("Verify code: ")
+        result = self.get_oauth_token(verifier)
+        return result
+      except KeyboardInterrupt:
+        pass
+    return {"error": "Unauthorize"}
 
 if __name__ == '__main__':
   print("Twitter Authentication (OAuth) module")
