4e6fd28a8edbe899cd5c1290d117df3d9d2c6418
Thanawat Petchuen <thanawat1petchuen@gmail.com>
2020-03-09 17:31:06 +0700

    [ADD] upload operation

--- pytweeper/operation.py
+++ pytweeper/operation.py
@@ -5,6 +5,9 @@ import shutil
 import os
 import pickle
 from progress.bar import ChargingBar
+from pathlib import Path
+
+from .drive import Drive
 
 def read(file):
   with open(file, 'rb') as f:
@@ -38,11 +41,18 @@ def mapper(d):
     else:
       return ''
 
-def read_and_download(file, path):
+def extract_data(file):
   data = read(file)
-
   mapped = list(map(mapper, data))
   merged = list(itertools.chain(*mapped))
+  return merged
+
+def list_files(path):
+  onlyfiles = [f for f in Path.iterdir(path) if Path.is_file(Path.joinpath(path, f))]
+  return onlyfiles
+
+def read_and_download(file, path):
+  merged = extract_data(file)
 
   make_dir(path)
   bar = ChargingBar('Downloading', max=len(merged))
@@ -51,5 +61,35 @@ def read_and_download(file, path):
     bar.next()
   bar.finish()
 
+def read_and_upload(source, folder_name=None):
+  d = Drive()
+  d.authenticate()
+  if type(source) == str:
+    source = Path(source)
+  files = list_files(source)
+  source_folder = source.parts[-1]
+
+  # Select folder section
+  if (folder_name):
+    folder = d.check_folder_if_exist(folder_name)
+    if folder:
+      folder_id = d.create_folder(source_folder, folder).get('id')
+      d.select_folder([folder_id])
+    else:
+      new_folder = d.create_folder(folder_name).get('id')
+      folder_id = d.create_folder(source_folder, [new_folder]).get('id')
+      d.select_folder([folder_id])
+  else:
+    folder_id = d.create_folder(source_folder).get('id')
+    d.select_folder([folder_id])
+  
+  # Upload section
+  bar = ChargingBar('Uploading', max=len(files))
+  for f in files:
+    file_name = f.parts[-1]
+    upload = d.upload_file(f, file_name)
+    bar.next()
+  bar.finish()
+
 if __name__ == '__main__':
-  print('filter file!')
+  print('Operation file!')
